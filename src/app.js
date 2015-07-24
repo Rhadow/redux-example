8037d0839b6fbe33da0d4ec247b7f9c08fc9563b
Rhadow <howard3433@gmail.com>
2015-07-25 00:18:08 +0800

    added initial webpack setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>Hello World!</div>
+		);
+	}
+}
+
+React.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var Webpack = require('webpack'),
+    path = require('path');
+
+var eslintrcPath = path.resolve(__dirname, '.eslintrc'),
+    nodeModulesPath = path.resolve(__dirname, 'node_modules'),
+    buildPath = path.resolve(__dirname, 'build'),
+    mainPath = path.resolve(__dirname, 'src', 'app.js');
+
+var config = {
+    devtool: 'eval',
+    watch: true,
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        mainPath
+    ],
+    output: {
+        path: buildPath,
+        filename: 'bundle.js',
+        publicPath: '/build/'
+    },
+    module: {
+        preLoaders: [{
+            test: /\.js(x)?$/,
+            loader: 'eslint',
+            exclude: nodeModulesPath
+        }],
+        loaders: [{
+            test: /\.js(x)?$/,
+            loader: 'babel',
+            exclude: nodeModulesPath
+        }, {
+            test: /\.(css|scss)$/,
+            loaders: ['style', 'css', 'sass']
+        }, {
+            test: /\.(png|jpg|jpeg|gif|svg)$/,
+            loader: 'url?limit=8192'
+        }, {
+            test: /\.(woff|woff2|ttf|eot)$/,
+            loader: 'url'
+        }]
+    },
+    plugins: [
+        new Webpack.HotModuleReplacementPlugin(),
+        new Webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css', '.scss']
+    },
+    eslint: {
+        configFile: eslintrcPath
+    }
+};
+
+module.exports = config;
