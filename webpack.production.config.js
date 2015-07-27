6bbd1f44a7e3c49aa23f1e095f7c307b933ab62f
Howard Chang <howard3433@gmail.com>
2015-07-27 15:57:53 +0800

    added deploy webpack confid and updated index.html

---
+++ webpack.production.config.js
@@ -0,0 +1,64 @@
+var Webpack = require('webpack'),
+    path = require('path'),
+    util = require('util'),
+    pkg = require('./package.json'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var nodeModulesPath = path.resolve(__dirname, 'node_modules'),
+    mainPath = path.resolve(__dirname, 'src', 'app.js'),
+    buildPath = path.resolve(__dirname, 'dist'),
+    cssBundleName = util.format('style.bundle.%s.css', pkg.version),
+    jsBundleName = util.format('js/app.bundle.%s.js', pkg.version);
+
+var config = {
+    devtool: 'source-map',
+    entry: {
+        app: [mainPath]
+    },
+    output: {
+        path: buildPath,
+        filename: jsBundleName
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js(x)?$/,
+                loader: 'babel',
+                exclude: nodeModulesPath
+            },
+            {
+                test: /\.(css|scss)$/,
+                loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap')
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: 'url?limit=8192&name=assets/images/[name].[ext]'
+            },
+            {
+                test : /\.(woff|woff2|ttf|eot|svg)$/,
+                loader: 'url?limit=8192&name=assets/fonts/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin(cssBundleName, {
+            allChunks: true
+        }),
+        new Webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'root.jQuery': 'jquery'
+        }),
+        new Webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css', '.scss']
+    }
+};
+
+module.exports = config;
