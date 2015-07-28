8f7f7dadb2884a5ad1e54d7d456f81f9370ee688
Rhadow <howard3433@gmail.com>
2015-07-28 22:19:57 +0800

    fix request logic

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -15,26 +15,23 @@ export function getFakeWeatherData(city) {
 
 export function getWeatherData(...queryConditions) {
 	let url = 'http://api.openweathermap.org/data/2.5/weather?units=metric';
-	switch (queryConditions.length) {
-		case 1:
-		    url += `&q=${queryConditions[0]}`;
-		    break;
-		case 2:
-		    url += `&lat=${queryConditions[0]}&lon=${queryConditions[1]}`;
-		    break;
-		default:
-		    url = '';
+	if (queryConditions.length === 1 && queryConditions[0]) {
+		url += `&q=${queryConditions[0]}`;
+	} else if (queryConditions.length === 2) {
+		url += `&lat=${queryConditions[0]}&lon=${queryConditions[1]}`;
+	} else {
+		url = '';
 	}
-	let promise = new Promise((resolve, reject) => {
-		request(url, (err, res) => {
-			if (err || res.body.cod !== 200) {
-				reject(err);
-			} else {
-				resolve(res.body);
-			}
-		});
-	});
 	if (url) {
+		let promise = new Promise((resolve, reject) => {
+			request(url, (err, res) => {
+				if (err || res.body.cod !== 200) {
+					reject(err);
+				} else {
+					resolve(res.body);
+				}
+			});
+		});
 		return {
 			types: [GET_WEATHER_DATA, GET_WEATHER_DATA_SUCCESS, GET_WEATHER_DATA_FAIL],
 			promise: promise
