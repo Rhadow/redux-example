a3146774c11e993f9d601b97892fdc2c69e39e3a
Howard Chang <howard3433@gmail.com>
2015-07-29 11:03:49 +0800

    added devTools for demo

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,22 +1,41 @@
+// Styles
 import './_App';
 
+// Components
 import React, { Component } from 'react';
 import WeatherApp from '../WeatherApp/WeatherApp';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import * as reducers from '../../reducers';
 import { promiseMiddleware } from '../../middlewares';
 
+//DevTools
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 const reducer = combineReducers(reducers);
 const finalCreateStore = applyMiddleware(promiseMiddleware)(createStore);
+// const finalCreateStore = compose(
+// 	applyMiddleware(promiseMiddleware),
+// 	devTools(),
+// 	persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+// 	createStore
+// );
 const store = finalCreateStore(reducer);
 
 class App extends Component {
 	render() {
 		return (
-			<Provider store={store}>
-			    {() => <WeatherApp />}
-			</Provider>
+			<div>
+				<Provider store={store}>
+				    {() => <WeatherApp />}
+				</Provider>
+				{/*<DebugPanel top right bottom>
+				    <DevTools
+				        store={store}
+				        monitor={LogMonitor} />
+				</DebugPanel>*/}
+			</div>
 		);
 	}
 }
